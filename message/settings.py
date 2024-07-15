b36802543f081a76d531396d2d6d2a2bf6a7300d
RichaChauhan1989 <richasain1989@gmail.com>
2024-07-15 14:05:57 +1200

    update authorization in settings

--- message/settings.py
+++ message/settings.py
@@ -141,7 +141,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',  # Add this line
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ]
 }
 
